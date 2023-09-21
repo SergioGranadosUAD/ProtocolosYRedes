1168bfdd6b5ace74bb79964b60bd9d7454035cb6
ElGarkarGit <idv22c.sgranados@uartesdigitales.edu.mx>
2023-09-21 16:22:32 -0600

    Prueba de funcionamiento de cliente/servidor UDP
    It's a mess

--- pingTest/main.cpp
+++ pingTest/main.cpp
@@ -4,7 +4,9 @@
 #include <sstream>
 #include <chrono>
 
-
+void ConnectUdpClient();
+void SetupUdpServer();
+sf::IpAddress ParseIP(std::string ipString);
 
 int main()
 {
@@ -12,10 +14,10 @@ int main()
 	char c;
 	std::cin >> c;
 	if (c == 'c') {
-		ConnectTcpClient();
+		ConnectUdpClient();
 	}
 	else if (c == 's') {
-		SetupTcpServer();
+		SetupUdpServer();
 	}
 	else {
 		std::cout << "No se introdujo un valor valido." << std::endl;
@@ -26,8 +28,29 @@ int main()
 }
 
 void ConnectUdpClient() {
+	sf::UdpSocket socket;
+	socket.setBlocking(true);
+	sf::IpAddress clientIP = ParseIP("192.168.100.31");
+	std::optional<sf::IpAddress> serverIP = ParseIP("192.168.100.31");
 
+	char data[5] = "Hola";
+	if (socket.send(data, 5, clientIP, 25565) != sf::Socket::Status::Done) {
+		std::cout << "No se pudo enviar el mensaje" << std::endl;
+	}
+	else {
+		std::cout << "Message sent:" << data << std::endl;
+	}
 
+	unsigned short serverPort = 0;
+	char recData[14];
+	std::size_t received;
+	if (socket.receive(recData, 14, received, serverIP, serverPort) != sf::Socket::Status::Done) {
+		std::cout << "No se pudo recibir la informacion" << std::endl;
+	}
+	else {
+		std::cout << "Message received: " << recData << std::endl;
+	}
+	/*
 	sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 600), 32), "Ping test");
 	while (window.isOpen()) {
 		sf::Event event;
@@ -40,9 +63,46 @@ void ConnectUdpClient() {
 
 	window.clear();
 	window.display();
+	*/
 }
 
-void SetupTcpServer() {
+void SetupUdpServer() {
+	sf::UdpSocket socket;
+	unsigned short clientPort = 25564;
+	sf::IpAddress serverIP = ParseIP("192.168.100.31");
+	std::optional<sf::IpAddress> clientIP = ParseIP("192.168.100.31");
+	if (socket.bind(25565) != sf::Socket::Status::Done) {
+		std::cout << "No se pudo bindear el socket al puerto 25565." << std::endl;
+	}
+	else {
 
+	}
+
+	socket.setBlocking(true);
+	char recData[100];
+	std::size_t received;
+	if (socket.receive(recData, 100, received, clientIP, clientPort) == sf::Socket::Status::Done) {
+		std::cout << "Se ha recibido el mensaje: " << recData << std::endl;
+		
+	}
+
+	char data[14] = "Hola cliente.";
+	if (socket.send(data, 14, clientIP.value(), clientPort) != sf::Socket::Status::Done) {
+		std::cout << "No se pudo enviar el mensaje" << std::endl;
+	}
+	else {
+		std::cout << "Se envio el mensaje de vuelta: " << data << std::endl;
+	}
+	
+}
+
+sf::IpAddress ParseIP(std::string ipString) {
+	std::stringstream stream(ipString);
+	int bytes[4];
+	char dot;
+	for (int i = 0; i < 4; ++i) {
+		stream >> bytes[i] >> dot;
+	}
+	return sf::IpAddress(bytes[0], bytes[1], bytes[2], bytes[3]);
 }
 
