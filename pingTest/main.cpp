70ef4a54981c7d968791a3b5ab962748a3d5430a
ElGarkarGit <idv22c.sgranados@uartesdigitales.edu.mx>
2023-09-21 13:09:59 -0600

    Base del programa, main loop

--- pingTest/main.cpp
+++ pingTest/main.cpp
@@ -4,7 +4,45 @@
 #include <sstream>
 #include <chrono>
 
+
+
 int main()
 {
+	std::cout << "Escriba 'c' para conectarse como cliente, o 's' para iniciar un servidor." << std::endl;
+	char c;
+	std::cin >> c;
+	if (c == 'c') {
+		ConnectTcpClient();
+	}
+	else if (c == 's') {
+		SetupTcpServer();
+	}
+	else {
+		std::cout << "No se introdujo un valor valido." << std::endl;
+	}
+	
+
+	
+}
+
+void ConnectUdpClient() {
+
+
+	sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 600), 32), "Ping test");
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+	}
+
+	window.clear();
+	window.display();
+}
+
+void SetupTcpServer() {
 
 }
+
